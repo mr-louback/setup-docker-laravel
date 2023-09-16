a4b1ce526f6e56923c3f2cc141dff1905108799b
mr-louback <loubacked4work@outlook.com>
2023-09-16 16:00:22 -0300

    change: status background

--- resources/views/components/status-support.blade.php
+++ resources/views/components/status-support.blade.php
@@ -1,3 +1,3 @@
-<div class="inline px-3 py-1 text-sm font-normal rounded-full text-{{$color}}-500 gap-x-2 bg-{{$color}}-100/60">
+<div class="inline px-3 py-1 text-sm font-normal rounded-full text-{{$color}}-500 gap-x-2 bg-white-100/60">
     {{ $textStatus }}
 </div>
